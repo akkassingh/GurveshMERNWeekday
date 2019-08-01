081c29d6653df589d6b4cf568c17e96fced2847a
Akkas Singh <akkassingh@Akkass-MacBook-Air.local>
2019-08-01 08:57:58 +0530

    introduction to viewEngines

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.port || 8000;
+
+app.get('/',function(req,res){
+    res.status(200).send('Hi from the Express')
+})
+
+app.get('/home',function(req,res){
+    //Show the home page
+    res.status(200).send('Welcome to home')
+})
+
+app.listen(port, function(err, res){
+    if(err)throw err;
+    console.log('Server is running on port ',port);
+})
--- nodeApp/app.js
+++ nodeApp/app.js
@@ -4,15 +4,33 @@ const app = express();
 
 const port = process.env.port || 8000;
 
-app.get('/',function(req,res){
+
+//serve the static files
+app.use(express.static(__dirname + '/public'));
+
+//view Engine to be used
+app.set('view engine', 'ejs')
+
+//path for views
+app.set('views', './src/views');
+
+app.get('/', function (req, res) {
     res.status(200).send('Hi from the Express')
 })
 
-app.get('/home',function(req,res){
-    res.status(200).send('Welcome to home')
+app.get('/home', function (req, res) {
+    res.render('home')
 })
 
-app.listen(port, function(err, res){
-    if(err)throw err;
-    console.log('Server is running on port ',port);
+app.get('/welcome', function (req, res) {
+    res.render('welcome',{
+        name: 'gurvesh',
+        age: 12,
+        location: "Delhi"
+    })
+})
+
+app.listen(port, function (err, res) {
+    if (err) throw err;
+    console.log('Server is running on port ', port);
 })
