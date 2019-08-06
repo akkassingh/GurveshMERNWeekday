6f1e74bead20154ac0298d7350925917fca95f4f
Akkas Singh <akkassingh@Akkass-MacBook-Air.local>
2019-08-06 21:56:53 +0530

    updates

--- app.js
+++ app.js
@@ -4,16 +4,21 @@ const app = express();
 
 const port = process.env.port || 8000;
 
-app.get('/',function(req,res){
+app.get('/', function (req, res) {
     res.status(200).send('Hi from the Express')
 })
 
-app.get('/home',function(req,res){
+app.get('/home', function (req, res) {
     //Show the home page
     res.status(200).send('Welcome to home')
 })
 
-app.listen(port, function(err, res){
-    if(err)throw err;
-    console.log('Server is running on port ',port);
+app.get('/random', (err, res) => {
+    if (err) throw err
+    res.status(200).send(Math.random())
+});
+
+app.listen(port, function (err, res) {
+    if (err) throw err;
+    console.log('Server is running on port ', port);
 })
--- nodeApp/app.js
+++ nodeApp/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
@@ -14,12 +15,41 @@ app.set('view engine', 'ejs')
 //path for views
 app.set('views', './src/views');
 
-app.get('/', function (req, res) {
+//initialize data
+let final_menu = [
+    {name:'Home', link: '/home'},
+    {name:'Prodcuct', link: '/product'},
+    {name:'About us', link: '/about'}
+]
+app.get('/',(req, res) => {
     res.status(200).send('Hi from the Express')
 })
 
+app.get('/root', function (req, res) {
+    res.render('root',{
+        title: 'Page'
+    })
+})
+
 app.get('/home', function (req, res) {
-    res.render('home')
+    res.render('home',{
+        title: 'Home',
+        menu: final_menu
+    })
+})
+
+app.get('/product', function (req, res) {
+    res.render('product',{
+        title: 'Product',
+        menu: final_menu
+    })
+})
+
+app.get('/about', function (req, res) {
+    res.render('about',{
+        title: 'About Us',
+        menu: final_menu
+    })
 })
 
 app.get('/welcome', function (req, res) {
@@ -28,9 +58,25 @@ app.get('/welcome', function (req, res) {
         age: 12,
         location: "Delhi"
     })
-})
+});
+
+app.get('/fs', function (req, res) {
+    fs.readFile('root.html',function(err, result){
+        res.status(200).write(result)
+    })
+});
+
 
 app.listen(port, function (err, res) {
     if (err) throw err;
     console.log('Server is running on port ', port);
 })
+
+
+// const add = function(a,b){
+//     return a+b;
+// }
+
+// const add = (a,b) => {
+//     return a+b;
+// }
