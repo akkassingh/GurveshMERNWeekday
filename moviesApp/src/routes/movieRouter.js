64fb52164f71670a7077622052ea20c2e2a1488d
Akkas Singh <akkassingh@Akkass-MacBook-Air.local>
2019-08-23 07:36:22 +0530

    mongoConnection

--- moviesApp/src/routes/movieRouter.js
+++ moviesApp/src/routes/movieRouter.js
@@ -1,77 +1,53 @@
-const express =  require('express');
+const express = require('express');
 const movieRouter = express.Router();
+const mongodb = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017";
+const db_name = 'clasesdb';
 
 
-let movies = [{
-    "_id": "5ab12612f36d2879268f284a",
-    "name": "Black Panther",
-    "language": "ENGLISH",
-    "rate": 4.5,
-    "type": "Action Adventure Fantasy",
-    "imageUrl": "https://image.ibb.co/f0hhZc/bp.jpg"
-}, {
-    "_id": "5ab12666f36d2879268f2902",
-    "name": "Death Wish",
-    "language": "ENGLISH",
-    "type": "Action Crime Thriller",
-    "rate": 3.2,
-    "imageUrl": "https://image.ibb.co/gC9PfH/dw.jpg"
-}, {
-    "_id": "5ab12678f36d2879268f291d",
-    "name": "Coco",
-    "language": "ENGLISH",
-    "type": "Adventure Animation Family",
-    "rate": 5,
-    "imageUrl": "https://image.ibb.co/dQwWSx/coco.jpg"
-}, {
-    "_id": "5ab126b6f36d2879268f2943",
-    "name": "Avengers",
-    "language": "ENGLISH",
-    "type": "Actione",
-    "rate": 2,
-    "imageUrl": "https://www.hindustantimes.com/rf/image_size_960x540/HT/p2/2018/04/01/Pictures/_46a0b2c0-3590-11e8-8c5f-3c6cc031651e.jpg"
-}, {
-    "_id": "5ab4e66b0c1d2b27846c6407",
-    "name": "Black Friday",
-    "language": "ENGLISH",
-    "rate": 4.5,
-    "type": "Action Adventure Fantasy",
-    "imageUrl": "https://image.ibb.co/f0hhZc/bp.jpg"
-}, {
-    "_id": "5ab12686f36d2879268f2930",
-    "name": "Mission Impossible",
-    "language": "English",
-    "rate": 2.5,
-    "type": "Horror Thriller",
-    "imageUrl": "https://pre00.deviantart.net/5d3b/th/pre/f/2017/313/2/b/mission_impossible__dark_directive_teaser_poster_by_themadbutcher-dbt9wav.png"
-}, {
-    "_id": "5ab12698f36d2879268f293e",
-    "name": "Incredibles 2",
-    "language": "ENGLISH",
-    "type": "Animated",
-    "rate": 4,
-    "imageUrl": "http://static1.squarespace.com/static/588a4776f5e23132a09d23b2/588a4e91be65945e50a36c0e/5b24084baa4a999c88a9f277/1529088827756/tre.jpg"
-}]
-
-function router(menu){
+function router(menu) {
 
     movieRouter.route('/')
         .get((req, res) => {
-            res.render('movies', {
-                title: 'Movie Details',
-                menu: menu,
-                movies
+            mongodb.connect(url, (err, db) => {
+                if (err) throw err
+                const dbo = db.db(db_name);
+                dbo.collection('movies').find({}).toArray(
+                    (err, data) => {
+                        if (err) throw err
+                        res.render('movies', {
+                            title: 'Movie Details',
+                            menu: menu,
+                            movies: data
+                        })
+                    }
+                )
             })
         })
 
-    movieRouter.route('/details')
+    movieRouter.route('/details/:id')
         .get((req, res) => {
-            res.render('movieDetails', {
-                title: 'Movie Details',
-                menu: menu
+            const {
+                id
+            } = req.params;
+            mongodb.connect(url, (err, db) => {
+                if (err) throw err
+                const dbo = db.db(db_name);
+                dbo.collection('movies').findOne({
+                    _id: id
+                },(err, data) => {
+                        console.log('Data Received is ', data)
+                        if (err) throw err
+                        res.render('movieDetails', {
+                            title: 'Movie Details',
+                            menu: menu,
+                            movieDetails: data
+                        })
+                    }
+                )
             })
         })
 
-        return movieRouter
-    }
-    module.exports = router;
+    return movieRouter
+}
+module.exports = router;
