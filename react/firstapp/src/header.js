bab914624a75b7b10b383557eee2473d74abb6cc
akkas <akkassingh@gmail.com>
2019-09-10 20:57:39 +0530

    type of components

--- react/firstapp/src/header.js
+++ react/firstapp/src/header.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const Header = () => {
-    return (
-        <header>
-        This is the Header
-        </header>
-    )
+class Header extends Component {
+    render(){
+        // const mystyle = {
+        //     header :  {
+        //         background: 'teal'
+        //     },
+        //     logo: {  
+        //         color: 'white',
+        //         fontSize: '30px',
+        //         textAlign: 'center'
+        //     }
+        // }
+        return (
+            <header>
+            <div>Logo</div>
+            <center>
+                <input />
+            </center>
+            </header>
+        )
+    }
 }
+
+// const Header = () => {
+//     return (
+//         <header>
+//         This is the Header
+//         </header>
+//     )
+// }
 
 export default Header
