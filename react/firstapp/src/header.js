ef38785f3f09d48e5f6510c45bdee3572f263693
akkas <akkassingh@gmail.com>
2019-09-11 06:56:07 +0530

    React Methods

--- react/firstapp/src/header.js
+++ react/firstapp/src/header.js
@@ -1,24 +1,26 @@
 import React, {Component} from 'react';
+import './header.css'
 
 class Header extends Component {
+    Change(event){
+        console.log(event)
+        console.log('++++++++++++++++')
+        console.log(event.target)
+        console.log(event.target.value)
+    }
     render(){
-        // const mystyle = {
-        //     header :  {
-        //         background: 'teal'
-        //     },
-        //     logo: {  
-        //         color: 'white',
-        //         fontSize: '30px',
-        //         textAlign: 'center'
-        //     }
-        // }
         return (
+            <React.Fragment>
             <header>
-            <div>Logo</div>
+            <div className="logo" onClick={() => {
+                console.log('I am clicked')
+            }}>Logo</div>
             <center>
-                <input />
+                <input onChange={this.Change}/>
             </center>
+            <hr />
             </header>
+            </React.Fragment>
         )
     }
 }
