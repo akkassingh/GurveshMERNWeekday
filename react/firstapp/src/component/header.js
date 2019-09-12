f0fcafdeb7bf438073e02f91f862a7b54da99681
akkas <akkassingh@gmail.com>
2019-09-12 06:50:20 +0530

    introduction to state

--- react/firstapp/src/component/header.js
+++ react/firstapp/src/component/header.js
@@ -2,11 +2,17 @@ import React, {Component} from 'react';
 import './header.css'
 
 class Header extends Component {
-    Change(event){
-        console.log(event)
-        console.log('++++++++++++++++')
-        console.log(event.target)
+    constructor (props) {
+        super(props)
+        this.state = {
+            title : 'React App',
+            userInput : 'User Search'
+        }
+    }
+
+    inputChange(event){
         console.log(event.target.value)
+        this.setState({userInput : event.target.value ? event.target.value : 'User Search'})
     }
     render(){
         return (
@@ -14,9 +20,10 @@ class Header extends Component {
             <header>
             <div className="logo" onClick={() => {
                 console.log('I am clicked')
-            }}>Logo</div>
+            }}>{this.state.title}</div>
             <center>
-                <input onChange={this.Change}/>
+                <input onChange={this.inputChange.bind(this)}/>
+                <p>{this.state.userInput}</p>
             </center>
             <hr />
             </header>
--- react/firstapp/src/index.js
+++ react/firstapp/src/index.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import Header from './header'
+import Header from './component/header'
 
-const App = () => {
-    return (
-        <div>
+import data from './db.json'
+
+
+class App extends Component {
+    constructor(){
+        super()
+        
+        this.state = {
+            news: data
+        }
+    }
+    render(){
+        console.log('Data received >>>>',this.state.news)
+        return(
+            <div>
             <Header />
-            <h1>Hello World!!</h1>
-            <h1>this is line 2</h1>
-        </div>    
-    )
+            <h1>Welcome to the News APP</h1>
+            <h2>This is current News</h2>
+            </div>
+        )
+    }
 }
+
+
+
+
+
+// const App = () => {
+//     return (
+//         <div>
+//             <Header />
+//             <h1>Hello World!!</h1>
+//             <h1>this is line 2</h1>
+//         </div>    
+//     )
+// }
 
 
 ReactDOM.render(<App />, document.getElementById('root'))
