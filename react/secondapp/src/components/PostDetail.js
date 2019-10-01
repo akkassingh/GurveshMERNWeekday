ad7c22a132a985ebed2838650a8df00e16c0cef9
akkas <akkassingh@gmail.com>
2019-10-01 20:30:06 +0530

    secondReactApp

---
+++ react/secondapp/src/components/PostDetail.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class PostDetails extends Component{
+    render(){
+        return(
+            <div>
+            <h1> Posts Detail Route </h1> </div>
+        )
+    }
+}
+
+export default PostDetails;
---
+++ react/secondapp/src/components/Posts.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Posts extends Component{
+    render(){
+        return(
+            <div>Post Route</div>
+        )
+    }
+}
+
+export default Posts;
