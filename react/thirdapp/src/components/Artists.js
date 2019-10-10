a7bf0c5b5b95da8ecd1d54be12b324d1231322a1
akkas <akkassingh@gmail.com>
2019-10-10 07:19:17 +0530

    artistDetails

--- react/thirdapp/src/components/Artists.js
+++ react/thirdapp/src/components/Artists.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Header from './Header';
+import AlbumList from './albums';
 
-const Artist = () => {
-    return (
-        <h1> Artist Details</h1>
-    )
+const url_Artist = "http://localhost:8900/artists"
+
+class Artist extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            artists: ''
+        }
+    }
+
+    componentDidMount(){
+        // console.log(this.props.match.params.artistid)
+        fetch(`${url_Artist}/${this.props.match.params.artistid}`,{
+            method: 'GET'
+        }).then((response) => response.json())
+        .then((data) => {
+            this.setState({
+                artists: data
+            })
+        })
+    }
+
+    render(){
+        return(
+            <div>
+                <Header />
+                <div className="artist_bio">
+                    <div className="artist_image">
+                        <span style={{background: `url('/images/covers/${this.state.artists.cover}.jpg')`}}>
+                        </span>
+                    </div>
+                    <div className="bio">
+                        <h3>{this.state.artists.name}</h3>
+                        <div className="bio_text">
+                            {this.state.artists.bio}
+                        </div>
+                    </div>
+                    <AlbumList albumList={this.state.artists.albums}/>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Artist;
